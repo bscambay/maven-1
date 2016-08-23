265997c499b4db9d40f690a6324983b7fee81358
wormholeracing <andyjm9@gmail.com>
2016-08-23 09:06:51 -0400

    init

---
+++ my-app/src/test/java/org/ssa/ironyard/AppTest.java
@@ -0,0 +1,18 @@
+package org.ssa.ironyard;
+
+import org.junit.Test;
+import static org.junit.Assert.*;
+
+public class AppTest 
+{
+	@Test
+    public void testApp()
+    {
+        assertTrue( true );
+    }
+    
+	@Test
+    public void sillyTest(){
+    	assertFalse(false);
+    }
+}
